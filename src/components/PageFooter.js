74a2ce7cb101c76a1484833072cbeae42986d0e3
Ursa Kacar <kacar.ursa@gmail.com>
2021-06-30 07:56:46 +0200

    updates footer

--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -5,7 +5,7 @@ export default function PageFooter() {
   return (
     <footer id="footer">
       <ul className="copyright">
-        <li>&copy; Računalniške storitve in svetovanje, {config.authorName} s.p.</li>
+        <li>&copy; {config.authorName} s.p.</li>
       </ul>
     </footer>
   )
